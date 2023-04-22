51017d37b4feef287a3fcffdc82c1b5c59440ff6
Frasson <nicfra0710@gmail.com>
2023-04-22 19:55:58 -0300

    fix: wrong authorization definition on OPTIONS method

--- src/utils/handlers.js
+++ src/utils/handlers.js
@@ -1,31 +1,30 @@
 const handleError = (error = {}) => {
-    console.error(error);
-    return {
-      statusCode: error.code ? error.code : 500,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true,
-      },
-      body: JSON.stringify({
-        status: false,
-        message: error.message ? error.message : "Unexpected error",
-      }),
-    };
+  console.error(error);
+  return {
+    statusCode: error.code ? error.code : 500,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({
+      status: false,
+      message: error.message ? error.message : "Unexpected error",
+    }),
   };
-  
-  const handleReturn = ({ statusCode, body }) => {
-    return {
-      statusCode,
-      body: JSON.stringify(body, null, 2),
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true,
-      },
-    };
+};
+
+const handleReturn = ({ statusCode, body }) => {
+  return {
+    statusCode,
+    body: JSON.stringify(body, null, 2),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
   };
-  
-  module.exports = {
-    handleError,
-    handleReturn,
-  };
-  
+};
+
+module.exports = {
+  handleError,
+  handleReturn,
+};
